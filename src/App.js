ce60abcffc1002957fbffe77e0f03dd05dd75a69
mantejohannes <mokibelomante@gmail.com>
2023-07-25 13:05:04 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CountryList from './components/CountryList';
 import CountryDetail from './components/CountryDetail';
--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -6,6 +6,8 @@ import axios from 'axios';
 const CountryDetail = () => {
   const { countryCode } = useParams();
   const [countryData, setCountryData] = useState(null);
+  
+  
 
   useEffect(() => {
     axios
@@ -23,15 +25,55 @@ const CountryDetail = () => {
   }
 
   return (
-    <div>
-      <h1>{countryData.name.common}</h1>
-      <img src={countryData.flags.png} alt={`Flag of ${countryData.name.common}`} />
-      <p>Population: {countryData.population}</p>
-      <p>Region: {countryData.region}</p>
-      <p>Capital: {countryData.capital}</p>
+    <div className='container-details'>
+    <div className="container d-flex align-items-center justify-content-center">
+        <div className="card shadow" style={{ width: '1500px' }}>
+        <div className="row no-gutters">
+          <div className="col-md-4">
+            <div style={{ height: '100%' }}>
+              <img src={countryData.flags.png} className="card-img" alt={`Flag of ${countryData.name.common}`} style={{ objectFit: 'cover', height: '100%'}} />
+            </div>
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h2 className="card-title">{countryData.name.common}</h2>
+              
+              <div className="population-tld">
+              <p className="card-text"> Native Name: {countryData.nativeName}</p>
+              <p className="card-text-tld">Top Level Domain: {countryData.tld}</p>
+              </div>
 
-      {/* Add other details you want to display */}
+              <div className="population-tld">
+              <p className="card-text">Population: {countryData.population}</p>
+              <p className="card-text">Currency: {countryData.currencies[Object.keys(countryData.currencies)[0]].name}</p>
+
+              </div>
+
+              <div className="population-tld">
+              <p className="card-text">Region: {countryData.region}</p>
+              <p className="card-text-lang">Languages: {Object.values(countryData.languages)[0]}</p>
+
+              </div>
+              <p className="card-text">Sub Region: {countryData.subregion}</p>
+              <p className="card-text">Capital: {countryData.capital}</p>
+
+
+              <p className="card-text">Currency: {countryData.currencies[Object.keys(countryData.currencies)[0]].name}</p>
+
+              <p className="card-text">Border Countries: <br></br> {Object.values(countryData.borders).map((borderCountry, index) => (
+                  <span key={index} className="border-country">{borderCountry}</span>
+                ))}</p>
+              
+              
+
+             
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+    
   );
 };
 
