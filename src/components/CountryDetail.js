ed4ad422ff891ae3c568cbb27100cfc62569cb5d
Johannes Mante <Johannes.Mante@gijima.com>
2026-06-02 15:19:41 +0200

    Fixed country search and country details functionality

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { useParams,useHistory } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 const CountryDetail = () => {
   const { countryCode } = useParams();
   const [countryData, setCountryData] = useState(null);
   const history = useHistory();
-
-  
 
   useEffect(() => {
     axios
@@ -24,63 +22,67 @@ const CountryDetail = () => {
     return <div>Loading...</div>;
   }
 
-  const handleGoBack = () => {
-    history.goBack();
-  };
+  const nativeName = countryData.name?.nativeName
+    ? Object.values(countryData.name.nativeName)[0].common
+    : countryData.name.common;
+
+  const currency = countryData.currencies
+    ? Object.values(countryData.currencies)[0].name
+    : 'N/A';
+
+  const languages = countryData.languages
+    ? Object.values(countryData.languages).join(', ')
+    : 'N/A';
+
+  const borders = countryData.borders || [];
 
   return (
-    <div className='container-details'>
-            
-            <button className="back-button" onClick={handleGoBack}>Back</button>
+    <div className="container-details">
+      <button className="back-button" onClick={() => history.goBack()}>
+        Back
+      </button>
 
-    <div className="container d-flex align-items-center justify-content-center">
+      <div className="container d-flex align-items-center justify-content-center">
         <div className="card shadow" style={{ width: '1500px' }}>
-        <div className="row no-gutters">
-          <div className="col-md-4">
-            <div style={{ height: '100%' }}>
-              <img src={countryData.flags.png} className="card-img" alt={`Flag of ${countryData.name.common}`} style={{ objectFit: 'cover', height: '100%'}} />
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <img
+                src={countryData.flags?.png}
+                className="card-img"
+                alt={`Flag of ${countryData.name.common}`}
+                style={{ objectFit: 'cover', height: '100%' }}
+              />
             </div>
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h2 className="card-title">{countryData.name.common}</h2>
-              
-              <div className="population-tld">
-              <p className="card-text"> Native Name: {countryData.nativeName}</p>
-              <p className="card-text-tld">Top Level Domain: {countryData.tld}</p>
+
+            <div className="col-md-8">
+              <div className="card-body">
+                <h2 className="card-title">{countryData.name.common}</h2>
+
+                <p>Native Name: {nativeName}</p>
+                <p>Top Level Domain: {countryData.tld?.join(', ') || 'N/A'}</p>
+                <p>Population: {countryData.population}</p>
+                <p>Currency: {currency}</p>
+                <p>Region: {countryData.region}</p>
+                <p>Languages: {languages}</p>
+                <p>Sub Region: {countryData.subregion || 'N/A'}</p>
+                <p>Capital: {countryData.capital?.join(', ') || 'N/A'}</p>
+
+                <p>
+                  Border Countries: <br />
+                  {borders.length > 0
+                    ? borders.map((borderCountry, index) => (
+                        <span key={index} className="border-country">
+                          {borderCountry}
+                        </span>
+                      ))
+                    : 'No border countries'}
+                </p>
               </div>
-
-              <div className="population-tld">
-              <p className="card-text">Population: {countryData.population}</p>
-              <p className="card-text">Currency: {countryData.currencies[Object.keys(countryData.currencies)[0]].name}</p>
-
-              </div>
-
-              <div className="population-tld">
-              <p className="card-text">Region: {countryData.region}</p>
-              <p className="card-text-lang">Languages: {Object.values(countryData.languages)[0]}</p>
-
-              </div>
-              <p className="card-text">Sub Region: {countryData.subregion}</p>
-              <p className="card-text">Capital: {countryData.capital}</p>
-
-
-              <p className="card-text">Currency: {countryData.currencies[Object.keys(countryData.currencies)[0]].name}</p>
-
-              <p className="card-text">Border Countries: <br></br> {Object.values(countryData.borders).map((borderCountry, index) => (
-                  <span key={index} className="border-country">{borderCountry}</span>
-                ))}</p>
-              
-              
-
-             
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-    
   );
 };
 
--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -7,27 +7,33 @@ import '../components/styling.css';
 const CountryList = () => {
   const [countries, setCountries] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get('https://restcountries.com/v3.1/all')
-      .then(response => {
-        setCountries(response.data);
-        setFilteredCountries(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
-
-  const handleSearch = searchTerm => {
-    const filtered = countries.filter(country => {
-      return country.name.common.toLowerCase().includes(searchTerm.toLowerCase());
-    });
-    setFilteredCountries(filtered);
-  };
+  const [searchTerm, setSearchTerm] = useState('');
 
   const history = useHistory();
+
+  useEffect(() => {
+  axios
+    .get('https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,cca3')
+    .then(response => {
+      console.log('Countries Loaded:', response.data);
+      setCountries(response.data);
+      setFilteredCountries(response.data);
+    })
+    .catch(error => {
+      console.error('API Error:', error);
+    });
+}, []);
+
+  const handleSearch = e => {
+    const value = e.target.value;
+    setSearchTerm(value);
+
+    const filtered = countries.filter(country =>
+      country.name?.common?.toLowerCase().includes(value.toLowerCase())
+    );
+
+    setFilteredCountries(filtered);
+  };
 
   const handleCountryClick = countryCode => {
     history.push(`/country/${countryCode}`);
@@ -35,44 +41,56 @@ const CountryList = () => {
 
   return (
     <div>
-      <h4>Where in the world</h4>
+      <h4>Where in the world?</h4>
+
       <div>
         <input
           type="text"
           placeholder="Search for a country"
-          onChange={e => handleSearch(e.target.value)}
-          className='search'
+          value={searchTerm}
+          onChange={handleSearch}
+          className="search"
         />
       </div>
+
+      <p>Results found: {Array.isArray(filteredCountries) ? filteredCountries.length : 0}</p>
+
       <div className="card-container">
-        {filteredCountries.map(country => (
-          <MDBCard key={country.cca3} className="country-card">
-            <div className="flag-container">
-              <MDBCardImage
-                src={country.flags.png}
-                position='top'
-                alt={`Flag of ${country.name.common}`}
-                className="flag-image"
-              />
-              <div className="outline" />
-            </div>
-            <MDBCardBody className="country-details">
-              <MDBCardTitle>{country.name.common}</MDBCardTitle>
-              <MDBCardText>
-                Population: {country.population}
-                <br />
-                Region: {country.region}
-                <br />
-                Capital: {country.capital}
-              </MDBCardText>
-              <MDBBtn className="linking">
-                <Link className="font-link"  to={`/country/${country.cca3}`} onClick={() => handleCountryClick(country.cca3)}>
-                  See More Info
-                </Link>
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        ))}
+        {Array.isArray(filteredCountries) &&
+          filteredCountries.map(country => (
+            <MDBCard key={country.cca3} className="country-card">
+              <div className="flag-container">
+                <MDBCardImage
+                  src={country.flags?.png}
+                  position="top"
+                  alt={`Flag of ${country.name?.common}`}
+                  className="flag-image"
+                />
+              </div>
+
+              <MDBCardBody className="country-details">
+                <MDBCardTitle>{country.name?.common}</MDBCardTitle>
+
+                <MDBCardText>
+                  Population: {country.population?.toLocaleString()}
+                  <br />
+                  Region: {country.region || 'N/A'}
+                  <br />
+                  Capital: {country.capital ? country.capital[0] : 'N/A'}
+                </MDBCardText>
+
+                <MDBBtn className="linking">
+                  <Link
+                    className="font-link"
+                    to={`/country/${country.cca3}`}
+                    onClick={() => handleCountryClick(country.cca3)}
+                  >
+                    See More Info
+                  </Link>
+                </MDBBtn>
+              </MDBCardBody>
+            </MDBCard>
+          ))}
       </div>
     </div>
   );
